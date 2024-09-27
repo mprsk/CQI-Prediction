053ef73d5d3f4d35f48063699c018670ffed1833
mprsk <147973477+mprsk@users.noreply.github.com>
2024-09-27 23:41:16 +0530

    Update DataPreparation.m

--- DataPreparation.m
+++ DataPreparation.m
@@ -8,7 +8,7 @@ r = [-15, -5]; % Range of noise power in dB
 
 fileID = fopen('channel_parameter_simulator.exp','w');
 fprintf(fileID,'#!/usr/bin/expect -f\n');
-fprintf(fileID,'\n\n# Developed by Wireless Communications and Navigation Lab, IIT Jodhpur (Author: Kiran M. P. R. S.)\n');
+fprintf(fileID,'\n\n# Developed by Wireless Communications and Navigation Lab, IIT Jodhpur\n');
 fprintf(fileID,'# Automated expect script generated for channel parameter variation using MATLAB\n');
 fprintf(fileID,'# This script continuously varies the DL noise power in the range [-15 dB, -5 dB] thus inducing CQI variations\n');
 fprintf(fileID,'# This is used for CQI training dataset generation later used for Bi-LSTM model training\n\n\n');
